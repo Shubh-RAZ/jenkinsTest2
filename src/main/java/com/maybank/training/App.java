ba5429c063cf8675e52d2b2279b451eccbe229f8
Shubh-RAZ <ce19b030@iittp.ac.in>
2025-08-13 12:24:10 +0000

    build trigger 3

--- src/main/java/com/maybank/training/App.java
+++ src/main/java/com/maybank/training/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Test Jenkins Build Triiger 2!" );
+        System.out.println( "Test Jenkins Build Triiger 3!" );
     }
 }
